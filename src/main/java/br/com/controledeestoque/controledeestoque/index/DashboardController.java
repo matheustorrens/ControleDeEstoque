77af274f604799cade58ca5de89f6d6fb5337b2d
matheustorrens <matheus.cacemiro@gmail.com>
2024-05-24 23:21:35 -0300

    Criando nova função para salvar usuário

--- src/main/java/br/com/controledeestoque/controledeestoque/index/DashboardController.java
+++ src/main/java/br/com/controledeestoque/controledeestoque/index/DashboardController.java
@@ -2,6 +2,7 @@ package br.com.controledeestoque.controledeestoque.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("index.html")
@@ -11,6 +12,11 @@ public class DashboardController {
     @GetMapping("")
     public void dashboard() {
 
+    }
+
+    @PostMapping("/salvarUsuario")
+    public void salvarUsuario() {
+        System.out.println("\n\n Eu amo a Manu \n\n");
     }
     
 }
